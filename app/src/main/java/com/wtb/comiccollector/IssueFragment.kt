6295d0c55f4251c92deb48c2328142ae386d39eb
wbenica <wbenica@calpoly.edu>
2021-01-27 17:22:01 -0800

    added a DatePickerFragment added release date to IssueFragment

--- app/src/main/java/com/wtb/comiccollector/IssueFragment.kt
+++ app/src/main/java/com/wtb/comiccollector/IssueFragment.kt
@@ -14,13 +14,25 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import java.io.File
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 private const val TAG = "IssueFragment"
+
+// Bundle Argument Tags
 private const val ARG_ISSUE_ID = "issue_id"
 private const val ARG_EDITABLE = "open_as_editable"
+
+// onActivityResult Request Codes
 private const val PICK_COVER_IMAGE = 0
+private const val RESULT_DATE_PICKER = 107
 private const val RESULT_NEW_SERIES = 108
+
+// Fragment Tags
+private const val DIALOG_NEW_SERIES = "DialogNewSeries"
+private const val DIALOG_DATE = "DialogDate"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -28,7 +40,8 @@ private const val RESULT_NEW_SERIES = 108
  * create an instance of this fragment.
  */
 // TODO: Do I need a separate fragment for editing vs viewing or can I do it all in this one?
-class IssueFragment : Fragment() {
+class IssueFragment : Fragment(),
+    DatePickerFragment.Callbacks {
     private lateinit var issue: Issue
     private lateinit var series: Series
     private lateinit var seriesList: List<Series>
@@ -49,6 +62,9 @@ class IssueFragment : Fragment() {
     private lateinit var inkersBox: TableLayout
     private lateinit var inkerEditText: EditText
     private lateinit var addInkerButton: ImageButton
+
+    private lateinit var releaseDateTextView: TextView
+
     private lateinit var toggleEditButton: ImageButton
     private lateinit var coverFile: File
     private lateinit var coverUri: Uri
@@ -92,6 +108,7 @@ class IssueFragment : Fragment() {
         addPencillerButton = view.findViewById(R.id.add_penciller_button) as ImageButton
         inkersBox = view.findViewById(R.id.inkers_box) as TableLayout
         inkerEditText = view.findViewById(R.id.issue_inker) as EditText
+        releaseDateTextView = view.findViewById(R.id.release_date_text_view)
         toggleEditButton = view.findViewById(R.id.edit_button) as ImageButton
         addInkerButton = view.findViewById(R.id.add_inker_button) as ImageButton
         addInkerButton = view.findViewById(R.id.add_inker_button) as ImageButton
@@ -117,7 +134,7 @@ class IssueFragment : Fragment() {
                         Log.d(TAG, "series list is zero")
                         val d = NewSeriesDialogFragment()
                         d.setTargetFragment(this@IssueFragment, RESULT_NEW_SERIES)
-                        d.show(parentFragmentManager, "NDF")
+                        d.show(parentFragmentManager, DIALOG_NEW_SERIES)
                     } else if (issue.seriesId == NEW_SERIES_ID) {
                         issue.seriesId = seriesList[0].seriesId
                     }
@@ -176,7 +193,14 @@ class IssueFragment : Fragment() {
         addSeriesButton.setOnClickListener {
             val d = NewSeriesDialogFragment()
             d.setTargetFragment(this@IssueFragment, RESULT_NEW_SERIES)
-            d.show(parentFragmentManager, "NDF")
+            d.show(parentFragmentManager, DIALOG_NEW_SERIES)
+        }
+
+        releaseDateTextView.setOnClickListener {
+            DatePickerFragment.newInstance(issue.releaseDate).apply {
+                setTargetFragment(this@IssueFragment, RESULT_DATE_PICKER)
+                show(this@IssueFragment.parentFragmentManager, DIALOG_DATE)
+            }
         }
 
         if (!isEditable) {
@@ -184,6 +208,7 @@ class IssueFragment : Fragment() {
             seriesSpinner.setSelection(indexOf)
 
         }
+
     }
 
     private fun toggleEnable() {
@@ -195,6 +220,7 @@ class IssueFragment : Fragment() {
         inkerEditText.isEnabled = !inkerEditText.isEnabled
         addInkerButton.isVisible = !addInkerButton.isVisible
         issueNumEditText.isEnabled = !issueNumEditText.isEnabled
+        releaseDateTextView.isEnabled = !releaseDateTextView.isEnabled
     }
 
     // TODO: Add textWatchers. as of now, they editTexts dont save anything
@@ -335,14 +361,18 @@ class IssueFragment : Fragment() {
 
     private fun updateUI() {
         seriesSpinner.setSelection(seriesList.indexOf(series))
-        issueNumEditText.setText(if (this.issue.issueNum == Int.MAX_VALUE) {
-            "1"
-        } else {
-            this.issue.issueNum.toString()
-        })
+        issueNumEditText.setText(
+            if (this.issue.issueNum == Int.MAX_VALUE) {
+                "1"
+            } else {
+                this.issue.issueNum.toString()
+            }
+        )
         writerEditText.setText(this.issue.writer)
         pencillerEditText.setText(this.issue.penciller)
         inkerEditText.setText(this.issue.inker)
+        releaseDateTextView.setText(this.issue.releaseDate.toString())
+        releaseDateTextView.setText(this.issue.releaseDate?.format(DateTimeFormatter.ofPattern("MMMM d, y")))
 
         this.issue.coverUri?.let {
             coverImageView.setImageURI(this.issue.coverUri)
@@ -376,6 +406,11 @@ class IssueFragment : Fragment() {
                     putSerializable(ARG_EDITABLE, openAsEditable)
                 }
             }
+    }
+
+    override fun onDateSelected(date: LocalDate) {
+        issue.releaseDate = date
+        updateUI()
     }
 }
 
